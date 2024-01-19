cabed3353ee515d9e793c91445cc9e3d2ac3490a
aaryan-rampal <76913929+aaryan-rampal@users.noreply.github.com>
2024-01-18 16:27:05 -0800

    Finish unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,48 +2,63 @@ import { useState } from 'react'
 
 const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>
 
-const Header = ({good, neutral, bad}) => {
+const Header = ({ good, neutral, bad }) => {
   return (
     <div>
       <h2>give feedback</h2>
       <Button onClick={good} text='good'></Button>
       <Button onClick={neutral} text='neutral'></Button>
       <Button onClick={bad} text='bad'></Button>
-  </div>
+    </div>
   )
 }
 
-const Stat = ({name, amt}) => {
+const StatisticLine = ({ name, amt }) => {
   return (
-      <p>{name}: {amt}</p>
+    <tr>
+      <td>{name}</td>
+      <td>{amt}</td>
+    </tr>
   )
 }
 
 
-const Footer = ({good, neutral, bad}) => {
+const Footer = ({ good, neutral, bad }) => {
   const computeAverage = () => {
-    // this only works since good, neutral, and bad are positive values
-    if (good + neutral + bad == 0) return 'no reviews yet'
-    let net = good - bad 
-    return net / (good+bad+neutral)
+    let net = good - bad
+    return net / (good + bad + neutral)
   }
 
   const computePositive = () => {
-    if (good + neutral + bad == 0) return 'no reviews yet'
-    return 100 * good / (good+neutral+bad) + '%'
+    return 100 * good / (good + neutral + bad) + '%'
   }
 
-  return (
-    <div>
-      <h2>Statistics</h2>
-      <Stat name='good' amt={good}></Stat>
-      <Stat name='neutral' amt={neutral}></Stat>
-      <Stat name='bad' amt={bad}></Stat>
-      <Stat name='all' amt={good + neutral + bad}></Stat>
-      <Stat name='average' amt={computeAverage()}></Stat>
-      <Stat name='positive' amt={computePositive()}></Stat>
-    </div>
-  )
+  // this only works since good, neutral, and bad are positive values
+  if (good + neutral + bad == 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <table>
+          <tbody>
+            <StatisticLine name='good' amt={good}></StatisticLine>
+            <StatisticLine name='neutral' amt={neutral}></StatisticLine>
+            <StatisticLine name='bad' amt={bad}></StatisticLine>
+            <StatisticLine name='all' amt={good + neutral + bad}></StatisticLine>
+            <StatisticLine name='average' amt={computeAverage()}></StatisticLine>
+            <StatisticLine name='positive' amt={computePositive()}></StatisticLine>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
 }
 
 const App = () => {
@@ -55,7 +70,7 @@ const App = () => {
   const handleGood = () => {
     // console.log('good button pressed')
     // console.log('old good', good)
-    setGood(good+1);
+    setGood(good + 1);
     // console.log('new good', good)
 
   }
@@ -64,8 +79,8 @@ const App = () => {
 
   return (
     <div>
-      <Header good={handleGood} bad={handleBad} neutral={handleNeutral}/>
-      <Footer good={good} bad={bad} neutral={neutral}/>
+      <Header good={handleGood} bad={handleBad} neutral={handleNeutral} />
+      <Footer good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
